1afd397358aaf530344d33efc9e0cf57dd5780ec
shady-abdelhamid <shady.abdelhamid@outlook.com>
2019-11-25 23:19:36 +0200

    Add bootstrap theme
    1. add atarter template to app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-product-management';
+  title = 'product management app';
 }
